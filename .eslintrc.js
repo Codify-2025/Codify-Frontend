4361c997c28a7e1a71516c67c4cceb0f0d600cfa
siiion <siwonjeon234@gmail.com>
2025-05-14 16:41:10 +0900

    FEAT: Vite 설정 파일 업데이트

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,18 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "react"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react'],
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
   ],
   rules: {
-    "react/react-in-jsx-scope": "off",
+    'react/react-in-jsx-scope': 'off',
   },
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
 };
