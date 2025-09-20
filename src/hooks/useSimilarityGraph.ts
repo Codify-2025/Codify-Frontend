d0a0aec92b1bcdec457a843d58fb788973efa018
siiion <siwonjeon234@gmail.com>
2025-09-20 16:17:41 +0900

    fix: 변경된 부분 수정

--- src/hooks/useSimilarityGraph.ts
+++ src/hooks/useSimilarityGraph.ts
@@ -1,14 +1,13 @@
 import { fetchGraph, fetchGraphRequest } from '@services/result';
 import { useQuery } from 'react-query';
 import type { graphMapped } from 'types/result';
-import { isDemo } from '@utils/demo';
 
 export const useSimilarityGraph = (params: fetchGraphRequest) => {
   const enabled =
     Number.isFinite(params.assignmentId) && Number.isFinite(params.week);
 
   return useQuery<graphMapped>({
-    queryKey: ['similarityGraph', demoOn, params.assignmentId, params.week],
+    queryKey: ['similarityGraph', params.assignmentId, params.week],
     queryFn: () => fetchGraph(params),
     enabled,
     staleTime: 1000 * 60 * 5,
